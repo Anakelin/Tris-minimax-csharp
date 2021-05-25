fa2d832175d1499336d4c8524744f94273e12706
Anakelin <77017350+Anakelin@users.noreply.github.com>
2021-05-25 18:31:54 +0200

    Aggiunte a State
    Commentato e trasferito la stampa del campo in State in quanto sembrava più logico.

--- minimax/minimax/tictactoe/State.cs
+++ minimax/minimax/tictactoe/State.cs
@@ -4,10 +4,12 @@ namespace minimax.tictactoe
 {
     public class State
     {
+        //Attributi
         public const int EMPTY = -1,  CROSS = (int)Player.Cross, CIRCLE = (int)Player.Circle;
         private int[,] _board;
         private Player _currentPlayer;
-            
+        
+        //Costruttori
         public State()
         {
             _board = new int[3,3];
@@ -26,6 +28,7 @@ namespace minimax.tictactoe
             _currentPlayer = player;
         }
 
+        //Inizializazione casuale del campo
         public void RandomState()
         {
             int num;
@@ -39,7 +42,49 @@ namespace minimax.tictactoe
                 }
             }
         }
+        
+        //Stampa del campo corrente
+        public void PrintBoard()
+        {
+            string[,] writeBoard = new string[3, 3];
 
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    switch (_board[row, col])
+                    {
+                        case -1:
+                            writeBoard[row, col] = " ";
+                            break;
+                        case 0:
+                            writeBoard[row, col] = "X";
+                            break;
+                        case 1:
+                            writeBoard[row, col] = "O";
+                            break;
+
+                    }
+                }
+
+            }
+            Console.WriteLine("╔═════════╦═════════╦═════════╗");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine($"║    {writeBoard[0, 0]}    ║    {writeBoard[0, 1]}    ║    {writeBoard[0, 2]}    ║");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine("╠═════════╬═════════╬═════════╣");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine($"║    {writeBoard[1, 0]}    ║    {writeBoard[1, 1]}    ║    {writeBoard[1, 2]}    ║");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine("╠═════════╬═════════╬═════════╣");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine($"║    {writeBoard[2, 0]}    ║    {writeBoard[2, 1]}    ║    {writeBoard[2, 2]}    ║");
+            Console.WriteLine("║         ║         ║         ║");
+            Console.WriteLine("╚═════════╩═════════╩═════════╝");
+            
+        }
+        
+        //Get di tutti gli attributi
         public Player Get_currentPlayer()
         {
             return _currentPlayer;
