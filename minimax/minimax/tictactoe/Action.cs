7feb4c455858cc261debe231bcd52e1648f6124d
only-gb77 <gabry.suzzi@gmail.com>
2021-05-25 11:29:38 +0200

    Correzione sintassi.

--- minimax/minimax/tictactoe/Action.cs
+++ minimax/minimax/tictactoe/Action.cs
@@ -4,12 +4,12 @@ namespace minimax.tictactoe
 	public class Action
 	{
 		public int row;
-		public int culumn;
+		public int column;
 
-		public Action(int row, int culumn)
+		public Action(int row, int column)
 		{
 			this.row=row;
-			this.culumn=culumn;
+			this.column=column;
 		}
 
 		public int row
@@ -17,9 +17,9 @@ namespace minimax.tictactoe
 			get { return row; }
 		}
 	
-		public int culumn
+		public int column
 		{
-			get { retur+n culumn; }
+			get { return column; }
 		}
 	}
 }
