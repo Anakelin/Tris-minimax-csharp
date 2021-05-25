3063d2db77fba124049f847818e51907e15f4ebb
Anakelin <77017350+Anakelin@users.noreply.github.com>
2021-05-25 11:45:36 +0200

    Create Game.cs
    Creazione di Game.cs.

---
+++ minimax/minimax/tictactoe/Game.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using minimax.core.adversarial;
+
+namespace minimax.tictactoe
+{
+    public class Game : IGame<State, Action, Player>
+    {
+            public List<Action> GetActions(State state)
+            {
+                throw new NotImplementedException();
+            }
+                public State GetInitialState()  
+            {
+                throw new NotImplementedException();
+            } 
+                public Player GetPlayer(State state)
+            {
+                throw new NotImplementedException();
+            }
+                public Player[] GetPlayers()
+            {
+                throw new NotImplementedException();
+            }
+                public State GetResult(State state, Action action)
+            {
+                throw new NotImplementedException();
+            }
+                public double GetUtility(State state,Player player)
+            {
+                throw new NotImplementedException();
+            }
+            public bool IsTerminal(State state)
+            {
+                throw new NotImplementedException();
+            }
+    }
+}
