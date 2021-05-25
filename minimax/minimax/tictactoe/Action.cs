685752a20f87949e415e15f84cd881fe518d61f4
only-gb77 <gabry.suzzi@gmail.com>
2021-05-25 11:25:54 +0200

    Aggiornamento Action.cs
    Creazione del costruttore e dei get.

--- minimax/minimax/tictactoe/Action.cs
+++ minimax/minimax/tictactoe/Action.cs
@@ -3,8 +3,23 @@ namespace minimax.tictactoe
 {
 	public class Action
 	{
-		public Action()
+		public int row;
+		public int culumn;
+
+		public Action(int row, int culumn)
 		{
+			this.row=row;
+			this.culumn=culumn;
+		}
+
+		public int row
+		{
+			get { return row; }
+		}
+	
+		public int culumn
+		{
+			get { retur+n culumn; }
 		}
 	}
 }
