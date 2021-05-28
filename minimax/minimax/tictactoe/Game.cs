421c57676aa686f76b8c9b42ab0e05b91a5f3dec
Anakelin <77017350+Anakelin@users.noreply.github.com>
2021-05-28 09:56:31 +0200

    Used Empty instead of -1

--- minimax/minimax/tictactoe/Game.cs
+++ minimax/minimax/tictactoe/Game.cs
@@ -14,7 +14,7 @@ namespace minimax.tictactoe
             {
                 for (int col = 0; col < 3; col++)
                 {
-                    if (board[row,col] == -1)
+                    if (board[row,col] == State.EMPTY)
                     {
                         legalMoves.Add(new Action(row,col));
                     }
@@ -38,7 +38,7 @@ namespace minimax.tictactoe
                     default: Console.Write("ERRORE: Dovresti inserire "); break;
                 }
             } while (loop);
-            return -1;
+            return State.EMPTY;
         }
         public State GetInitialState()  
         {
@@ -83,7 +83,7 @@ namespace minimax.tictactoe
             if (IsTerminal(state))
             {
                 winner = IsVictory(state);
-                if (winner != -1)
+                if (winner != State.EMPTY)
                 {
                     if (winner == (int)player)
                     {
@@ -141,7 +141,7 @@ namespace minimax.tictactoe
 
             int isWin = IsVictory(state);
 
-            if (isWin != -1)
+            if (isWin != State.EMPTY)
             {
                 return true;
             }
@@ -151,7 +151,7 @@ namespace minimax.tictactoe
                 {
                     for (int col = 0; col < 3; col++)
                     {
-                        if (board[row, col] == -1)
+                        if (board[row, col] == State.EMPTY)
                         {
                             return false;
                         }
@@ -191,7 +191,7 @@ namespace minimax.tictactoe
                 }
             }
             
-            return -1;
+            return State.EMPTY;
         }
 
         public double IsNearVictory(State state)
@@ -199,7 +199,7 @@ namespace minimax.tictactoe
             int[,] board = state.Get_board();
             int currentPlayer = (int)state.Get_currentPlayer();
             int winner = NearVictory2(board);
-            if (winner != -1)
+            if (winner != State.EMPTY)
             {
                 if (winner == currentPlayer)
                 {
@@ -226,34 +226,34 @@ namespace minimax.tictactoe
             {
                 for (int row = 0; row < 3; row++)
                 {
-                    bool rowComb0 = board[row, 0] == (int)player && board[row, 1] == (int)player && board[row, 2] == -1;
-                    bool rowComb1 = board[row, 0] == (int)player && board[row, 2] == (int)player && board[row, 1] == -1;
-                    bool rowComb2 = board[row, 1] == (int)player && board[row, 2] == (int)player && board[row,0]== -1;
+                    bool rowComb0 = board[row, 0] == (int)player && board[row, 1] == (int)player && board[row, 2] == State.EMPTY;
+                    bool rowComb1 = board[row, 0] == (int)player && board[row, 2] == (int)player && board[row, 1] == State.EMPTY;
+                    bool rowComb2 = board[row, 1] == (int)player && board[row, 2] == (int)player && board[row,0]== State.EMPTY;
                     rowComb = rowComb0 || rowComb1 || rowComb2 || rowComb;
                 }
 
                 for (int col = 0; col < 3; col++)
                 {
-                    bool colComb0 = board[0, col] == (int)player && board[1, col] == (int)player && board[2, col] == -1;
-                    bool colComb1 = board[0, col] == (int)player && board[2, col] == (int)player && board[1, col] == -1;
-                    bool colComb2 = board[1, col] == (int)player && board[2, col] == (int)player && board[0, col] == -1;
+                    bool colComb0 = board[0, col] == (int)player && board[1, col] == (int)player && board[2, col] == State.EMPTY;
+                    bool colComb1 = board[0, col] == (int)player && board[2, col] == (int)player && board[1, col] == State.EMPTY;
+                    bool colComb2 = board[1, col] == (int)player && board[2, col] == (int)player && board[0, col] == State.EMPTY;
                     colComb = colComb0 || colComb1 || colComb2 || colComb;
                 }
 
-                bool diaComb = (board[0, 0] == (int)player && board[1, 1] == (int)player && board[2, 2] == -1) || (board[0, 0] == (int)player && board[2, 2] == (int)player && board[1, 1] == -1) || (board[1, 1] == (int)player && board[2, 2] == (int)player && board[0, 0] == -1);
-                bool diaCombR = (board[0, 2] == (int)player && board[1, 1] == (int)player && board[2, 0] == -1) || (board[0, 2] == (int)player && board[2, 0] == (int)player && board[1, 1] == -1) || (board[1, 1] == (int)player && board[2, 0] == (int)player && board[0, 2] == -1);
+                bool diaComb = (board[0, 0] == (int)player && board[1, 1] == (int)player && board[2, 2] == State.EMPTY) || (board[0, 0] == (int)player && board[2, 2] == (int)player && board[1, 1] == State.EMPTY) || (board[1, 1] == (int)player && board[2, 2] == (int)player && board[0, 0] == State.EMPTY);
+                bool diaCombR = (board[0, 2] == (int)player && board[1, 1] == (int)player && board[2, 0] == State.EMPTY) || (board[0, 2] == (int)player && board[2, 0] == (int)player && board[1, 1] == State.EMPTY) || (board[1, 1] == (int)player && board[2, 0] == (int)player && board[0, 2] == State.EMPTY);
 
                 if (rowComb || colComb || diaComb || diaCombR)
                 {
                     return (int)player; 
                 }
             }
-            return -1;
+            return State.EMPTY;
         }
 
         public int ReturnInt()
         {
-            int n = -1;
+            int n = State.EMPTY;
             bool loop;
             do
             {
